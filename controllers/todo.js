de26e0a11961fc2a7748ad736e49a7fa36676303
Dmitry Kulakov <atomeistee@gmail.com>
2023-05-20 11:24:57 +0300

    todos CRUD

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,25 +1,69 @@
-const todo = require("../model/todo");
+const Todo = require("../model/todo");
 
-exports.getTodo = async (req, res) => {
-  // todo.find({ title: req.title }, (err, docs) => {
-  //   if (err) {
-  //     res.status(500).send({ status: "failed", message: err });
-  //   } else {
-  //     res.send({
-  //       status: "success",
-  //       message: "Data fetched correctly",
-  //       data: docs,
-  //     });
-  //   }
-  // });
-
+exports.getTodos = async (req, res) => {
   try {
-    const todos = await todo.find({ title: "test" });
+    const todos = await Todo.find({});
     console.log(todos);
     res.send({
       status: "success",
       message: "Data fetched correctly",
       data: todos,
+    });
+  } catch (err) {
+    res.status(500).send({ status: "failed", message: err });
+  }
+};
+exports.createTodo = async (req, res) => {
+  try {
+    const body = req.body;
+    const createdTodo = new Todo({
+      title: body.title,
+      isComplete: body.isComplete,
+      description: body.description,
+      createdAt: body.createdAt,
+    });
+    await createdTodo.save();
+    res.send({
+      status: "success",
+      message: "Todo created successfully",
+      data: createdTodo,
+    });
+  } catch (err) {
+    res.status(500).send({ status: "failed", message: err });
+  }
+};
+
+exports.updateTodo = async (req, res) => {
+  try {
+    const body = req.body;
+    const todo = await Todo.updateOne(
+      { _id: body._id },
+      {
+        title: body.title,
+        isComplete: body.isComplete,
+        description: body.description,
+        createdAt: body.createdAt,
+      }
+    );
+
+    res.send({
+      status: "success",
+      message: "Todo updated successfully",
+      data: todo,
+    });
+  } catch (err) {
+    res.status(500).send({ status: "failed", message: err });
+  }
+};
+exports.deleteTodo = async (req, res) => {
+  try {
+    const body = req.body;
+    const todo = await Todo.deleteOne({ _id: req.params.id });
+
+    res.send({
+      status: "success",
+      message: "Todo deleted successfully",
+      data: todo,
     });
   } catch (err) {
     res.status(500).send({ status: "failed", message: err });
--- routes/todo.js
+++ routes/todo.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getTodo } = require("../controllers/todo");
-router.get("/", getTodo);
+const {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require("../controllers/todo");
+// /todo
+router.get("/", getTodos);
+router.post("/", createTodo);
+router.put("/", updateTodo);
+router.delete("/:id", deleteTodo);
+
 module.exports = router;
